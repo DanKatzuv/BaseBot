afe0d93aae30572844baaf2939c1adcb94af4af9
Paulo Khayat <paulokhayat@gmail.com>
2018-12-06 15:36:26 +0200

    Copy all methods from Drivetrain

--- src/main/java/robot/subsystems/drivetrain/DrivetrainSimulation.java
+++ src/main/java/robot/subsystems/drivetrain/DrivetrainSimulation.java
@@ -8,17 +8,65 @@
 package robot.subsystems.drivetrain;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import robot.Robot;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class DrivetrainSimulation extends Subsystem {
+public class DrivetrainSimulation extends Drivetrain {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    public DrivetrainSimulation(){
+
+    }
 
     @Override
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
+    }
+
+    public void setSpeed(double leftSpeed, double rightSpeed) {
+        setLeftSpeed(leftSpeed);
+        setRightSpeed(rightSpeed);
+    }
+
+    public double getLeftSpeed(){
+        return 0;
+    }
+
+    private void setLeftSpeed(double speed) {
+
+    }
+
+    public double getRightSpeed() {
+        return 0;
+    }
+
+    private void setRightSpeed(double speed) {
+    }
+
+    public double getRightDistance() {
+        return 0;
+    }
+
+    public double getLeftDistance(){
+        return 0;
+    }
+
+    public void resetEncoders() {
+
+    }
+
+    public double getAngle() {
+        return 0;
+    }
+
+    public double getPitch() {
+        return 0;
+    }
+
+    public double getRoll() {
+        return 0;
     }
 }
