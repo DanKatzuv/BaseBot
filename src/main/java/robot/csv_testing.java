ce45d0f9fd6637c169c5f1ddc06cc4831cddc27f
orel-david <orel.david1502@gmail.com>
2018-12-21 13:26:50 +0200

    Add a testing file to the csv class

--- src/main/java/robot/csv_testing.java
+++ src/main/java/robot/csv_testing.java
@@ -1,5 +1,17 @@
 package robot;
 
+import java.io.FileNotFoundException;
+
 public class csv_testing {
-    public static void main(args[])
+    private static CSV csv;
+
+    public static void main(String args[]) {
+        try {
+            csv = new CSV("random.csv", "number");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        csv.update(22);
+        csv.close();
+    }
 }
