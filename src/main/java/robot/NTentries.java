4728cd1a87ab1bc49823f1dae2196da6acabdaf3
orel-david <orel.david1502@gmail.com>
2018-12-27 19:23:33 +0200

    Add the setup for the networktables

--- src/main/java/robot/NTentries.java
+++ src/main/java/robot/NTentries.java
@@ -1,14 +1,25 @@
 package robot;
 
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class NTentries {
     private int amount;
+
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    NetworkTable table = inst.getTable("dataTable");
     NetworkTableEntry[] entries = new NetworkTableEntry[amount];
+    String[] names = new String[amount];
     double[] data = new double[amount];
 
-    NTentries(int amountParameters) {
+    NTentries(int amountParameters, String... names) {
         this.amount = amountParameters;
+        int count = 0;
+        for (String name : names) {
+            this.names[count] = name;
+            count++;
+        }
     }
 
     public void insertDoubles(double... data) {
@@ -30,6 +41,7 @@ public class NTentries {
     public void zip() {
         int count = 0;
         while (count < this.amount) {
+            entries[count] = table.getEntry(names[count]);
             entries[count].setDouble(data[count]);
             count++;
         }
