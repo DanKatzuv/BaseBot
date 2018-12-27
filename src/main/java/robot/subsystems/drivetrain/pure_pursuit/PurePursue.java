bdc1b2830078c7eaddf85efbc8270056297464e7
lior <lior5858@gmail.com>
2018-12-27 19:27:44 +0200

    Change isFinished
    a bit more sensitive, stops a b it eraly

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/PurePursue.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/PurePursue.java
@@ -75,8 +75,8 @@ public class PurePursue extends Command {
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
 //        return false;
-        boolean closeToLast = (drivetrain.currentLocation.getX() >= path.getWaypoint(path.length() - 1).getX() - 0.1 &&
-                drivetrain.currentLocation.getY() >= path.getWaypoint(path.length() - 1).getY() - 0.1);
+        boolean closeToLast = (drivetrain.currentLocation.getX() >= path.getWaypoint(path.length() - 1).getX() - 0.3 &&
+                drivetrain.currentLocation.getY() >= path.getWaypoint(path.length() - 1).getY() - 0.3);
         return (closeToLast &&
                 drivetrain.getLeftSpeed() < Constants.STOP_SPEED_THRESH &&
                 drivetrain.getRightSpeed() < Constants.STOP_SPEED_THRESH);
