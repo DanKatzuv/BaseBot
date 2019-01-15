148c666aec4d5760eb4164a9f6428470bede44e9
lior <lior5858@gmail.com>
2019-01-15 17:34:16 +0200

    Adjusted constants for 2018 robot

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     static final boolean LEFT_REVERSED = false;
     static final boolean RIGHT_REVERSED = true;
     public static final double MAX_RATE = 0.3;
-    public static final double DISTANCE_PER_PULSE = (0.2032 * Math.PI) / 231;//diameter of the wheel is 0.2032 meters (8 inches), the encoder sends 226 pulses every 360 degree turn
+    public static final double DISTANCE_PER_PULSE = 0.003451;
+    public static final double NINTEEN_DISTANCE = (0.2032 * Math.PI) / 231;//diameter of the wheel is 0.2032 meters (8 inches), the encoder sends 226 pulses every 360 degree turn
 
 }
