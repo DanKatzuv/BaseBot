ca9379cc147c5456f9bed54838401d72e3e30c5a
lior <lior5858@gmail.com>
2019-01-15 22:11:11 +0200

    Adjust constants for more precise movement

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     //DRIVING CONSTANTS
 
     public final static double LOOKAHEAD_DISTANCE = 0.45; //in meters
-    public static final double MAX_VELOCITY = 3.762;
+    public static final double MAX_VELOCITY = 3.2;//3.762
 
     //PATH GENERATION CONSTANTS
     public static final double SPACING_BETWEEN_WAYPOINTS = 0.1524; //meters
@@ -15,15 +15,15 @@ public class Constants {
     public final static double TOLERANCE = 0.001;
 
     //VELOCITY CONSTANTS
-    public static final double MAX_PATH_VELOCITY = 1.7;
-    public static final double MAX_ACCEL = 0.6;
+    public static final double MAX_PATH_VELOCITY =1.2;
+    public static final double MAX_ACCEL = 0.5;
 
-    public static final double K_CURVE = 3; //number from 1 to 5
+    public static final double K_CURVE = 4; //number from 1 to 5
 
     //DRIVING CONSTANTS (pure numbers)
     public final static double kV = 1/MAX_VELOCITY;
-    public final static double kA = 0.004;
-    public final static double kP = 0.01;
+    public final static double kA = 0.001;
+    public final static double kP = 0.05;
 
     public final static double STOP_SPEED_THRESH = 0.1; //the speed the robot could stop at the end of the path.
 
